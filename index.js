ee8deec1cf11ae8adb6713a01b5615927fd321cb
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-16 15:50:25 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
